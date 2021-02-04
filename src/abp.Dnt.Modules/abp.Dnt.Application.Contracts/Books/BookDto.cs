3461569a8d6a555b67cf62443fbc05d4121dcf43
Đạt <datnt3009@gmail.com>
2021-02-04 10:02:12 +0700

    Refactoring architecture and add MsBuild

---
+++ src/abp.Dnt.Modules/abp.Dnt.Application.Contracts/Books/BookDto.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Application.Dtos;
+
+namespace abp.Dnt.Application.Contracts.Books
+{
+    public class BookDto : AuditedEntityDto
+    {
+        public string Name { get; set; }
+
+        public BookType MyProperty { get; set; }
+    }
+}
